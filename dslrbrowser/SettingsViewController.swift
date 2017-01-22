3e4f79dccaad3f683351435001015b1e3da32698
Andras Bekesi <andras.bekesi@bikeonet.hu>
2017-01-22 10:24:43 +0100

    request authorization for use of Location services

--- dslrbrowser/SettingsViewController.swift
+++ dslrbrowser/SettingsViewController.swift
@@ -7,17 +7,28 @@
 //
 
 import Foundation
+import CoreLocation
 
-class SettingsViewController:UITableViewController {
+class SettingsViewController:UITableViewController, CLLocationManagerDelegate {
 
     @IBOutlet var switchOnlyCanon:UISwitch?
     @IBOutlet var switchInsertGPS:UISwitch?
+    let locationManager:CLLocationManager = CLLocationManager()
 
     override func viewDidLoad() {
         super.viewDidLoad()
         let prefs = UserDefaults.standard
         switchOnlyCanon?.setOn(prefs.bool(forKey: "useOnlyCanon"), animated: false)
         switchInsertGPS?.setOn(prefs.bool(forKey: "insertGPS"), animated: false)
+        locationManager.delegate = self
+        
+        locationManager.requestWhenInUseAuthorization()
+
+        let status:CLAuthorizationStatus = CLLocationManager.authorizationStatus()
+        if (status == CLAuthorizationStatus.authorizedAlways ||
+            status == CLAuthorizationStatus.authorizedWhenInUse ) {
+                switchInsertGPS?.isEnabled = true
+        }
     }
     
     @IBAction func switchOnlyCanonChanged(sender: UISwitch) {
@@ -25,5 +36,19 @@ class SettingsViewController:UITableViewController {
         prefs.set(sender.isOn, forKey: "useOnlyCanon")
     }
     
+    @IBAction func switchInsertGPSChanged(sender: UISwitch) {
+        let prefs = UserDefaults.standard
+        prefs.set(sender.isOn, forKey: "insertGPS")
+    }
+    
+    func locationManager(_ manager: CLLocationManager,
+                                  didChangeAuthorization status: CLAuthorizationStatus) {
+
+        if (status == CLAuthorizationStatus.authorizedAlways ||
+            status == CLAuthorizationStatus.authorizedWhenInUse ) {
+            switchInsertGPS?.isEnabled = true
+        }
+        
+    }
     
 }
